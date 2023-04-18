aad883ba0a5d26cdab7c878ca6e12f956edff09a
lazySir <969060742@qq.com>
2023-04-19 00:08:04 +0800

    404
    1.添加404回到首页地址

--- src/views/404.vue
+++ src/views/404.vue
@@ -12,7 +12,7 @@
         <div class="bullshit__info">发生了一些错误，您可能无法访问此页面。</div>
         <div class="bullshit__headline">{{ message }}</div>
         <div class="bullshit__info">请检查您输入的URL是否正确，或点击下面的按钮返回主页。</div>
-        <a href="" class="bullshit__return-home">回到首页</a>
+        <a href="./" class="bullshit__return-home">回到首页</a>
       </div>
     </div>
   </div>
