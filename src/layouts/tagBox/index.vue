58d3f252a83dbf30a6d8bac0debb8a716765af52
lazySir <969060742@qq.com>
2023-04-28 21:13:12 +0800

    switch组件抽离引用未修改
    1.BUG修复：抽离switch组件时未将tagBox里的引用进行修改
    2.优化：将要使用的state进行解构使用

--- src/layouts/tagBox/index.vue
+++ src/layouts/tagBox/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="tagBox-container">
-    <el-tag v-for="(item, index) in tagList" :effect="headerStore.nightTheme" :type='headerStore.nightClass' :key="item.title" @click="tagBoxStore.pushRoute(item.name)" @close="tagBoxStore.deleteTag(index)" :closable="index !== 0">
+    <el-tag v-for="(item, index) in tagList" :effect="nightTheme" :type='nightClass' :key="item.title" @click="tagBoxStore.pushRoute(item.name)" @close="tagBoxStore.deleteTag(index)" :closable="index !== 0">
       <span v-if="item.icon != ''">
         <component style="width: 12px" :is="item.icon"></component>
       </span>
@@ -11,12 +11,12 @@
 </template>
 <script lang="ts" setup>
 import { useTagBoxStore } from '@/store/tagBox'
-import { useHeaderStore } from '@/store/header'
+import { useSwitchStore } from '@/store/switch'
 import { storeToRefs } from 'pinia'
 const tagBoxStore = useTagBoxStore()
-const { tagList } = storeToRefs(tagBoxStore)
-const headerStore = useHeaderStore()
-const { night } = storeToRefs(headerStore)
+const { tagList ,deleteTag} = storeToRefs(tagBoxStore)
+const switchStore = useSwitchStore()
+const { nightTheme,nightClass } = storeToRefs(switchStore)
 </script>
 <style scoped>
 @import './index.scss'
