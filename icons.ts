9343e6bb2d548b0b4e4b85f4e9314750e77be3ed
lazySir <969060742@qq.com>
2023-05-06 00:26:26 +0800

    将icons图标抽离
    1.将icons图标抽离为菜单列表新增可选图标提供条件

---
+++ icons.ts
@@ -0,0 +1,10 @@
+export default [
+  'i-material-symbols:lock',
+  'i-fluent:data-histogram-24-filled',
+  'i-material-symbols:home',
+  'i-ep:message-box',
+  'i-basil:other-1-outline',
+  'i-material-symbols:person',
+  'i-carbon:user-avatar-filled',
+  'i-material-symbols:grid-view',
+]
--- uno.config.ts
+++ uno.config.ts
@@ -1,20 +1,13 @@
 // uno.config.ts
 import { defineConfig } from 'unocss'
 import presetIcons from '@unocss/preset-icons'
-
+import icons from './icons'
 export default defineConfig({
   presets: [
-    presetIcons({ /* options */ }),
+    presetIcons({
+      /* options */
+    }),
     // ...other presets
   ],
-  safelist:[
-    'i-material-symbols:lock',
-    'i-fluent:data-histogram-24-filled',
-    'i-material-symbols:home',
-    'i-ep:message-box',
-    'i-basil:other-1-outline',
-    'i-material-symbols:person',
-    'i-carbon:user-avatar-filled',
-    'i-material-symbols:grid-view',
-  ]
+  safelist: [...icons],
 })
