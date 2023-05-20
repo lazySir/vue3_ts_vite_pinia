3c0d62e459c5a6ac04d35deb87cc27a3f495b2fd
lazySir <969060742@qq.com>
2023-05-21 04:21:28 +0800

    国际化
    1.将elementUI组件默认为中文

--- src/utils/element-plus.ts
+++ src/utils/element-plus.ts
@@ -1,9 +1,14 @@
 //导入所有图标并进行全局注册。
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import ElementPlus from 'element-plus'
 export const setupElementPlus = (app: any) => {
   //注册图标
   for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
   }
+  app.use(ElementPlus, {
+    locale: zhCn,
+  })
 }
