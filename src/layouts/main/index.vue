34ef8ee5ac7113ff317cbaf5d3f9bcbfdc211253
lazySir <969060742@qq.com>
2023-05-21 14:27:36 +0800

    布局修改：为el-main添加卡片
    1.为el-main添加一个卡片 背景为白色

--- src/layouts/main/index.vue
+++ src/layouts/main/index.vue
@@ -1,7 +1,20 @@
 <template>
-  <el-scrollbar>
-    <router-view />
-  </el-scrollbar>
+  <div class="home-card">
+    <el-scrollbar>
+      <router-view />
+    </el-scrollbar>
+  </div>
 </template>
 <script lang="ts" setup></script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.home-card {
+  box-sizing: border-box;
+  padding: 20px;
+  height: 100%;
+  overflow-x: hidden;
+  background-color: #ffffff;
+  border: 1px solid #e4e7ed;
+  border-radius: 6px;
+  box-shadow: 0 0 12px #0000000d;
+}
+</style>
