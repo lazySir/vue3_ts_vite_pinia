98cba61c780ff7721e6df5247730b781c75f06d1
lazySir <969060742@qq.com>
2023-05-21 14:28:07 +0800

    home页面重构
    1.重构home页面 不会出现滚动条情况

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,20 +1,11 @@
 <template>
-  <el-card class="home">
-    <img class="home-bg" src="@/assets/images/welcome.png" alt="" />
-  </el-card>
+  <div class="home card">
+    <img class="home-bg" src="@/assets/images/welcome.png" alt="welcome" />
+  </div>
 </template>
-<script lang="ts" setup></script>
-<style lang="scss" scoped>
-.home {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  .home-bg {
-    width: 100%;
-    max-width: 1200px;
-    margin-bottom: 20px;
-  }
-}
+
+<script setup lang="ts" name="home"></script>
+
+<style scoped lang="scss">
+@import "./index.scss";
 </style>
