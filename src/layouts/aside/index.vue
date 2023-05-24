48199a6e83c05ebceef1f5668a20ab6b3f676ada
lazySir <969060742@qq.com>
2023-05-24 14:08:40 +0800

    aside组件logo固定
    1.固定logo组件使其不会滚动

--- src/layouts/aside/index.vue
+++ src/layouts/aside/index.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="aside-container">
-    <el-scrollbar>
-    <el-menu :collapse="asideStore.isCollapse" 
-    active-text-color="#009688" 
-    background-color="#ffffff" 
-    class="el-menu-vertical-demo" 
-    default-active="首页" 
-    text-color="#333333">
-      <!-- 头部 -->
+    <div class="logo-container">
       <logoForm />
-      <!-- 菜单 -->
-      <subMenu />
-    </el-menu>
-  </el-scrollbar>
+    </div>
+    <el-scrollbar class="scroll-container">
+      <el-menu :collapse="asideStore.isCollapse" 
+        active-text-color="#009688" 
+        background-color="#ffffff" 
+        class="el-menu-vertical-demo" 
+        default-active="首页" 
+        text-color="#333333">
+          <!-- 菜单 -->
+          <subMenu />
+      </el-menu>
+    </el-scrollbar>
   </div>
 </template>
+
 <script lang="ts" setup>
 import subMenu from '@/layouts/aside/components/subMenu.vue' //子菜单
 import logoForm from '@/layouts/aside/components/logo.vue'
