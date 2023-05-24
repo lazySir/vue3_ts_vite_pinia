f2832d71389fe6a4a2cb755e3d29e40b78e783ff
lazySir <969060742@qq.com>
2023-05-24 13:30:31 +0800

    F5刷新造成模式重置问题
    1.修复 使用localStorage存储

--- src/components/darkSwitch.vue
+++ src/components/darkSwitch.vue
@@ -1,12 +1,12 @@
 <template>
-  <el-switch @change='switchStore.changeDark' v-model="dark" class="mt-2" style="--el-switch-on-color: #0d0d0d; --el-switch-off-color: #8B8989" inline-prompt active-icon="Moon" inactive-icon="Sunny" />
+  <el-switch :disabled="os" @change='switchStore.changeDark' v-model="dark" class="mt-2" style="--el-switch-on-color: #0d0d0d; --el-switch-off-color: #8B8989" inline-prompt active-icon="Moon" inactive-icon="Sunny" />
 </template>
 
 <script setup lang="ts">
 import {useSwitchStore} from '@/store/switch'
 import {storeToRefs} from 'pinia'
 const switchStore = useSwitchStore()
-const {dark} = storeToRefs(switchStore)
+const {dark,os} = storeToRefs(switchStore)
 </script>
 <style scoped>
 .el-switch {
--- src/store/switch.ts
+++ src/store/switch.ts
@@ -1,15 +1,24 @@
 import { defineStore } from 'pinia'
+
 export const useSwitchStore = defineStore('switch', {
   state: () => {
+    // 从localStorage中获取初始状态
+    const dark = localStorage.getItem('dark') === 'true' ? true : false;
+    const os = localStorage.getItem('os') === 'true' ? true : false;
+
+    // 若是暗夜模式，需要立刻改变主题
+    if(dark) {
+      let html = document.documentElement
+      html.classList.add('dark')
+    }
+
     return {
-      dark: false, //暗色模式
-      os: false, //跟随系统
+      dark: dark,
+      os: os,
     }
   },
   getters: {
-    //当night为true时，返回success，否则返回info
     nightClass: (state) => (state.dark ? 'info' : 'success'),
-    //当night为true时，返回dark，否则返回light
     nightTheme: (state) => (state.dark ? 'dark' : 'light'),
   },
   actions: {
@@ -17,33 +26,28 @@ export const useSwitchStore = defineStore('switch', {
       //获取html根节点
       let html = document.documentElement
       this.dark ? html.classList.add('dark') : html.classList.remove('dark')
+      
+      //在localStorage中存储状态
+      localStorage.setItem('dark', this.dark ? 'true' : 'false')
     },
-
     changeOS() {
       const match = window.matchMedia('(prefers-color-scheme: dark)')
-
+    
       // 创建一个回调函数，当系统颜色主题变化时更新应用的颜色主题
       const updateTheme = (event: any) => {
         this.dark = event.matches
         this.changeDark()
       }
-
+    
       // 设置初始主题
       updateTheme(match)
-
+    
       // 添加事件监听器
       match.addEventListener('change', updateTheme)
-
-      //如果暗夜模式打开则关闭
-      if (this.dark) {
-        this.dark = !this.dark
-      }
-
-      //如果跟随系统关闭，则恢复light模式
-      if (!this.os) {
-        this.dark = false
-        this.changeDark()
-      }
+    
+      //在localStorage中存储状态
+      localStorage.setItem('os', this.os ? 'true' : 'false')
     },
+    
   },
 })
