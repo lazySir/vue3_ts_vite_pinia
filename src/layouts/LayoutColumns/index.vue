fac6db50391d6657d08fe33f431bbebd5b541378
lazySir <969060742@qq.com>
2023-05-25 01:17:05 +0800

    布局切换：完成剩余两种布局
    1.共完成四种布局切换
    2.移出鼠标移动到布局上的提示

--- src/layouts/LayoutColumns/index.vue
+++ src/layouts/LayoutColumns/index.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>我是LayoutColumns</div>
+  <div class="el-container">
+    <!-- Layout布局  -->
+    <el-container>
+      <!-- 左边 -->
+      <el-aside width="auto"><Aside /></el-aside>
+      <!-- 右边 -->
+      <el-container>
+        <!-- 头部 -->
+        <el-header><Header /> </el-header>
+        <tagBox v-if="globalStore.isShowTab"></tagBox>
+        <!-- 主体展示 -->
+        <el-main>
+          <Main />
+        </el-main>
+        <el-footer v-if="globalStore.isShowFooter"><Footer /></el-footer>
+      </el-container>
+    </el-container>
+  </div>
 </template>
-<script lang='ts' setup>
+<script lang="ts" setup>
+import Header from '@/layouts/components/header/index.vue'
+import Aside from '@/layouts/components/aside/index.vue'
+import Main from '@/layouts/components/main/index.vue'
+import tagBox from '@/layouts/components/tagBox/index.vue'
+import Footer from '@/layouts/components/footer/index.vue'
+import { useGolbalStore } from '@/store/global'
+const globalStore = useGolbalStore()
 </script>
 <style scoped>
+@import './index.scss';
 </style>
--- src/layouts/LayoutTransverse/index.vue
+++ src/layouts/LayoutTransverse/index.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>我是LayoutTransverse</div>
+  <div class="el-container">
+    <el-container>
+      <el-header class="header-container">
+        <logoForm class="logo"></logoForm>
+        <el-menu mode="horizontal" active-text-color="#009688" background-color="#ffffff" default-active="首页" text-color="#333333">
+          <!-- 菜单 -->
+          <subMenu />
+        </el-menu>
+        <HeaderR class="r-container"></HeaderR>
+      </el-header>
+      <tagBox v-if="global.isShowTab"></tagBox>
+      <el-main><Main></Main></el-main>
+      <el-footer v-if="global.isShowFooter"><Footer class="footer"></Footer></el-footer>
+    </el-container>
+  </div>
 </template>
-<script lang='ts' setup>
+<script lang="ts" setup>
+import logoForm from '@/layouts/components/logo/index.vue'
+import HeaderR from '@/layouts/components/header/right/index.vue'
+import subMenu from '@/layouts/components/aside/components/subMenu.vue'
+import Main from '@/layouts/components/main/index.vue'
+import Footer from '@/layouts/components/footer/index.vue'
+import tagBox from '@/layouts/components/tagBox/index.vue'
+import { useGolbalStore } from '@/store/global'
+const global = useGolbalStore()
 </script>
-<style scoped>
+<style lang="scss" scoped>
+@import './index.scss'
 </style>
